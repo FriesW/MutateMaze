59db71462e8b9d2764659cd9cebe6cb8b63bbb5b
FriesW <william.james.fries@gmail.com>
2017-08-12 00:42:47 -0500

    Added Distance mechanic to Nodes
    Some recusion which can go crazy if not used carefully. Still not sure
    how I feel about this particular implementation.

--- Node.py
+++ Node.py
@@ -11,8 +11,24 @@ class Node:
 	
 	def __init__(self):
 		self.adjacencys = [None, None, None, None]
-		
-	def set_node(self, dir, node):
+		self.distance = -1
+	
+	def get_distance(self):
+		if self.distance == -1:
+			raise LookupError("Distance has not yet been set.")
+		return self.distance
+	
+	def set_distance(self, src):
+		self.distance = src.get_distance() + 1
+		for adj in self.adjacencys:
+			other = adj.get_other_node(self)
+			if other != src:
+				other.set_distance(self)
+	
+	def set_raw_distance(self, dist):
+		self.distance = dist
+	
+	def set_node(self, node, dir):
 		adj = Adjacency(self, node)
 		self.adjacencys[dir] = adj
 		node.adjacencys[__inverse[dir]] = adj
